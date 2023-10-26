5942221964b344faaf8eaec9f95712c7d74712fd
HuiMun <jlijs0265@gmail.com>
2023-10-26 17:32:01 +0900

    [기능추가] 조직도 api, 이름으로 사용자 검색 api, 유저정보변경 api 기능 추가

--- src/main/java/com/codeplay/controller/UserInformationController.java
+++ src/main/java/com/codeplay/controller/UserInformationController.java
@@ -6,11 +6,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codeplay.domain.CriteriaVo;
 import com.codeplay.domain.UserVo;
 import com.codeplay.domain.userInformation.dto.UserInformationDto;
+import com.codeplay.domain.userInformation.dto.UserInformationPatchDto;
+import com.codeplay.domain.userInformation.dto.UserQueryDto;
+import com.codeplay.domain.userInformation.vo.UserInformationRequestVo;
 import com.codeplay.domain.userInformation.vo.UserInformationResponseVo;
 import com.codeplay.domain.userInformation.vo.UserQueryListResponseVo;
 import com.codeplay.domain.userInformation.vo.UserQueryResponseVo;
@@ -29,6 +35,8 @@ import lombok.extern.slf4j.Slf4j;
 public class UserInformationController {
 	@Autowired
 	UserInformationService service;
+	//일자 format
+
 
 	@Operation(summary = "user 사용자 정보 페이지 조회", description = "회원정보조회 페이지에서 user 의 사용자 정보를 조회할때 사용합니다.")
 	@Parameter(name = "user_no", description = "유저 개인을 식별하기위한 유저번호")
@@ -42,7 +50,7 @@ public class UserInformationController {
 		UserInformationResponseVo vo = new UserInformationResponseVo();
 		vo.setDept_name(user.getDept().getDept_name());
 		vo.setUser_address(user.getUser().getUser_address());
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		vo.setUser_birth_date(format.format(user.getUser().getUser_birth_date()));
 		vo.setUser_email(user.getUser().getUser_email());
 		vo.setUser_name(user.getUser().getUser_name());
@@ -54,23 +62,42 @@ public class UserInformationController {
 		return list;
 	}
 	
+	@Operation(summary = "user 사용자 정보 변경", description = "회원정보조회 페이지에서 user 의 사용자 정보의 변경사항을 저장할때 사용합니다.")
+	@Parameter(name = "user_no", description = "유저 개인을 식별하기위한 유저번호")
+	@PatchMapping("/user-information")
+	public void patch_user_information(@RequestParam int user_no, @RequestBody UserInformationRequestVo user) {
+		log.info("user-information에 호출함. user_no: {} request_body {}",user_no, user);
+		UserInformationPatchDto userPatch = new UserInformationPatchDto();
+		userPatch.setUser_no(user_no);
+		userPatch.setUser_address(user.getUser_address());
+		userPatch.setUser_phone(user.getUser_phone());
+		userPatch.setUser_profile(user.getUser_profile());
+		log.info("서비스로 전달한 데이터 userPatch: "+userPatch);
+		service.patchUser(userPatch);
+	}
+	
 	@Operation(summary = "user 사용자 정보 테이블 조회", description = "사용자 조회페이지의 user 의 사용자 정보를 테이블로 조회할때 사용합니다. 이름으로검색가능, pagenation가능")
 	@Parameter(name = "user_name", description = "유저 개인을 검색하기위한 유저이름")
 	@Parameter(name = "page", description = "pagenation에서 보여줄 현재 page number")
 	@Parameter(name = "limit", description = "pagenation에서 보여줄 최대 갯수")
 	@GetMapping("/user-query")
 	public List<UserQueryResponseVo> get_user_query(@RequestParam String user_name, @RequestParam Integer page, @RequestParam Integer limit) {
-		log.info("user-query에 호출함. user_name: {} page: {} limit: {}", user_name, page, limit);
+		log.info("user-query에 호출함. user_name: {} page: {} limit: {}", user_name, page-1, limit);
+		CriteriaVo cri = new CriteriaVo(user_name, page-1, limit);
 		List<UserQueryResponseVo> list = new ArrayList();
-		UserQueryResponseVo user = new UserQueryResponseVo();
-		user.setDept_name("개발팀");
-		user.setUser_birth_date("1960-01-24");
-		user.setUser_name("홍길동");
-		user.setUser_no(0);
-		user.setUser_position("연구원");
-		user.setUser_profile("https://picsum.photos/200");
-		log.info("서비스로부터 받아온 데이터 user: "+user);
-		list.add(user);
+		List<UserQueryDto> users = service.getUserByUsername(cri);
+		log.info("서비스로부터 받아온 데이터 user: "+users);
+		for(UserQueryDto userQuery : users) {
+			UserQueryResponseVo user = new UserQueryResponseVo();
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			user.setUser_birth_date(format.format(userQuery.getUser_birth_date()));
+			user.setUser_profile(userQuery.getUser_profile());
+			user.setDept_name(userQuery.getDept_name());
+			user.setUser_name(userQuery.getUser_name());
+			user.setUser_no(userQuery.getUser_no());
+			user.setUser_position(userQuery.getUser_position());
+			list.add(user);
+		}
 		return list;
 	}
 	@Operation(summary = "조직도 조회", description = "사용자 조회페이지의 조직도를 전체 조회할때 사용합니다. 1단으로 구성됨.")
@@ -78,13 +105,16 @@ public class UserInformationController {
 	public List<UserQueryListResponseVo> get_user_query_list() {
 		log.info("user-query-list에 호출함.");
 		List<UserQueryListResponseVo> list = new ArrayList();
-		UserQueryListResponseVo user = new UserQueryListResponseVo();
-		user.setDept_name("개발팀");
-		user.setUser_name("홍길동");
-		user.setUser_no(0);
-		user.setUser_position("연구원");
-		log.info("서비스로부터 받아온 데이터 user: "+user);
-		list.add(user);
+		List<UserQueryDto> users = service.getAllUser();
+		log.info("서비스로부터 받아온 데이터 user: "+users);
+		for(UserQueryDto userQuery : users) {
+			UserQueryListResponseVo user = new UserQueryListResponseVo();
+			user.setDept_name(userQuery.getDept_name());
+			user.setUser_name(userQuery.getUser_name());
+			user.setUser_no(userQuery.getUser_no());
+			user.setUser_position(userQuery.getUser_position());
+			list.add(user);
+		}
 		return list;
 	}
 
