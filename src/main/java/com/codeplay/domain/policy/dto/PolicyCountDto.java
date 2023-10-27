e7d5859c682751e56b975f3b8f97867f335121a1
HuiMun <jlijs0265@gmail.com>
2023-10-27 15:06:43 +0900

    [기능추가] 출/퇴근 정책 관련 api 기능 추가

--- src/main/java/com/codeplay/domain/policy/dto/PolicyCountDto.java
+++ src/main/java/com/codeplay/domain/policy/dto/PolicyCountDto.java
@@ -1,4 +1,4 @@
-package com.codeplay.domain.policy.vo;
+package com.codeplay.domain.policy.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PolicyCountResponseVo {
+public class PolicyCountDto {
 	 private Integer policy_no;
 	 private String policy_name;
 	 private Integer count;
