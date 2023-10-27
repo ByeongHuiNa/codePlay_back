bda4419773b57e3692da3e6bc1b5946dd0075a97
HuiMun <jlijs0265@gmail.com>
2023-10-27 17:49:07 +0900

    [코드변경] 자료형 수정

--- src/main/java/com/codeplay/domain/policy/vo/UserPolicyDetailRequestVo.java
+++ src/main/java/com/codeplay/domain/policy/vo/UserPolicyDetailRequestVo.java
@@ -1,6 +1,4 @@
-package com.codeplay.domain.policy.dto;
-
-import java.util.Date;
+package com.codeplay.domain.policy.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,15 +7,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PolicyUserDetailDto {
+public class UserPolicyDetailRequestVo {
 	 private Integer user_no;
-	 private String user_profile;
 	 private Integer policy_no;
-	 private String dept_name;
-	 private String user_name;
-	 private String user_position;
 	 private String standard_start_time;
 	 private String standard_end_time;
 	 private String policy_designated_date;
-
 }
