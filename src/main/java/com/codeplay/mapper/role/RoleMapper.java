1ef40b3e723a2e7e8735b74abc5b5626d4ba7bdd
HuiMun <jlijs0265@gmail.com>
2023-10-30 18:05:10 +0900

    [기능추가] 권한 관리 기능

---
+++ src/main/java/com/codeplay/mapper/role/RoleMapper.java
@@ -0,0 +1,20 @@
+package com.codeplay.mapper.role;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.codeplay.domain.Attendance_ManagerVo;
+import com.codeplay.domain.User_RoleVo;
+import com.codeplay.domain.role.dto.RoleCountDto;
+import com.codeplay.domain.role.dto.RoleQueryDto;
+import com.codeplay.domain.role.dto.RoleQueryListDto;
+
+@Mapper
+public interface RoleMapper {
+	public List<RoleQueryListDto> findByRoleQueryList(RoleQueryDto roleQuery);
+	public List<User_RoleVo> findByUserNo(int user_no);
+	public void deleteByUserNo(int user_no);
+	public List<Attendance_ManagerVo> findAttendMaByUserNo(int user_no);
+	public List<RoleCountDto> findRoleCount();
+}
