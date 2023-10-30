bc0da72e7ff3f08f7ebfd64a6e423b539aac8d9a
jj <wkdlwps21@naver.com>
2023-10-30 14:43:13 +0900

    [기능 추가] 캘린더 back 컨트롤러 및 스웨거 기능 테스트 완료

--- src/main/java/com/codeplay/service/calendar/CalendarServiceImpl.java
+++ src/main/java/com/codeplay/service/calendar/CalendarServiceImpl.java
@@ -1,8 +1,17 @@
 package com.codeplay.service.calendar;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codeplay.domain.Leave_ApprovalVo;
+import com.codeplay.domain.ScheduleVo;
+import com.codeplay.domain.Schedule_MemoVo;
+import com.codeplay.domain.UserVo;
+import com.codeplay.domain.calendar.vo.UserLeaveVo;
+import com.codeplay.domain.calendar.vo.UserScheduleLeaveMemoVo;
+import com.codeplay.domain.calendar.vo.UserScheduleVo;
 import com.codeplay.mapper.calendar.CalendarMapper;
 
 @Service
@@ -10,6 +19,84 @@ public class CalendarServiceImpl implements CalendarService {
 
 	@Autowired
 	CalendarMapper calendarMapper;
+
+	@Override
+	public List<ScheduleVo> getScheduleList(Long user_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getScheduleList(user_no);
+	}
+
+	@Override
+	public List<Leave_ApprovalVo> getLeaveList(Long user_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getLeaveList(user_no);
+	}
+
+	@Override
+	public void createSchedule(ScheduleVo schedule) {
+		// TODO Auto-generated method stub
+		calendarMapper.createSchedule(schedule);
+	}
+
+	@Override
+	public void updateSchedule(ScheduleVo schedule) {
+		// TODO Auto-generated method stub
+		calendarMapper.updateSchedule(schedule);
+	}
+
+	@Override
+	public void deleteSchedule(Long schedule_no) {
+		// TODO Auto-generated method stub
+		calendarMapper.deleteSchedule(schedule_no);
+	}
+
+	@Override
+	public List<UserScheduleVo> getShereScheduleList(Long user_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getShereScheduleList(user_no);
+	}
+
+	@Override
+	public List<UserLeaveVo> getShereLeaveList(Long user_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getShereLeaveList(user_no);
+	}
+
+	@Override
+	public List<Schedule_MemoVo> getScheduleMemoList(Long schedule_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getScheduleMemoList(schedule_no);
+	}
+
+	@Override
+	public List<Schedule_MemoVo> getLeaveMemoList(Long leave_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getLeaveMemoList(leave_no);
+	}
+
+	@Override
+	public UserVo getMemoDetail(Long memo_no) {
+		// TODO Auto-generated method stub
+		return calendarMapper.getMemoDetail(memo_no);
+	}
+
+	@Override
+	public void createMemo(Schedule_MemoVo memoVo) {
+		// TODO Auto-generated method stub
+		calendarMapper.createMemo(memoVo);
+	}
+
+	@Override
+	public void updateMemo(Schedule_MemoVo memoVo) {
+		// TODO Auto-generated method stub
+		calendarMapper.updateMemo(memoVo);
+	}
+
+	@Override
+	public void deleteMemo(Long memo_no) {
+		// TODO Auto-generated method stub
+		calendarMapper.deleteMemo(memo_no);
+	}
 	
 
 }
