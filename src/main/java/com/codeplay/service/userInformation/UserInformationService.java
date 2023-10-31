528de33239c55ce509975fe16cacd29a480f22a4
HuiMun <jlijs0265@gmail.com>
2023-10-31 17:39:49 +0900

    [기능추가] 부서전체 조회 api 기능 추가

--- src/main/java/com/codeplay/service/userInformation/UserInformationService.java
+++ src/main/java/com/codeplay/service/userInformation/UserInformationService.java
@@ -6,6 +6,7 @@ import com.codeplay.domain.CriteriaVo;
 import com.codeplay.domain.userInformation.dto.UserInformationDto;
 import com.codeplay.domain.userInformation.dto.UserInformationPatchDto;
 import com.codeplay.domain.userInformation.dto.UserQueryDto;
+import com.codeplay.domain.userInformation.vo.DeptListResponseVo;
 
 
 public interface UserInformationService {
@@ -17,7 +18,7 @@ public interface UserInformationService {
 	public List<UserQueryDto> getUserByUsername(CriteriaVo cri);
 	//User Table 수정하는 서비스
 	public void patchUser(UserInformationPatchDto userPatch);
-	
-	
 
+	//Dept 전체 가져오는 서비스
+    List<DeptListResponseVo> getAllDept();
 }
