a21d21f3726805ea4ce1278b233135b842c1d6f1
HuiMun <jlijs0265@gmail.com>
2023-11-07 18:21:54 +0900

    [TODO] 세션에 로그인데이터 담아야함.

--- src/main/java/com/codeplay/controller/Html5PathsController.java
+++ src/main/java/com/codeplay/controller/Html5PathsController.java
@@ -1,13 +1,16 @@
 package com.codeplay.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class Html5PathsController {
-
+    //TODO:세션에서 토큰 저장해야함.
     @RequestMapping(value = "/{path:[^\\.]*}")
     public String forward() {
+        log.info("forward 처리");
         return "forward:/index.html";
     }
 }
