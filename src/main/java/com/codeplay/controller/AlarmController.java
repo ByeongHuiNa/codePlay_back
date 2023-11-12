1aa3729f869f84e10d9b2d0321aee008c119954a
HUIMUN SONG <jlijs0265@gmail.com>
2023-11-12 22:04:45 +0900

    [기능추가]  알림기능추가 TODO:db에 알림클릭시 이동할 go_to_url 추가해야함

--- src/main/java/com/codeplay/controller/AlarmController.java
+++ src/main/java/com/codeplay/controller/AlarmController.java
@@ -1,5 +1,6 @@
 package com.codeplay.controller;
 
+import com.codeplay.domain.AlarmVo;
 import com.codeplay.service.access.AccessService;
 import com.codeplay.service.alarm.AlarmService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 
@@ -32,12 +30,21 @@ public class AlarmController {
         headers.add("X-Accel-Buffering", "no");
         return ResponseEntity.ok().headers(headers).body(sseEmitter);
     }
-    @Operation(summary = "alarm 기존값 전송", description = "alarm db에 있는 내용 전송")
-    @GetMapping("/get-alarm")
-    @Parameter(name = "user_no", description = "유저 개인을 식별하기위한 유저번호")
-    public ResponseEntity<Object> GetAlarm(@RequestParam int user_no) {
-        log.info("get-alarm 호출, 호출한 user_no : {}", user_no);
-        service.getAlarm(user_no);
-        return ResponseEntity.ok("초기데이터 호출성공");
+
+    @Operation(summary = "alarm stuts 변경", description = "alarm 목록 클릭시")
+    @Parameter(name = "alarm_no", description = "유저 개인을 식별하기위한 유저번호")
+    @PutMapping(value = "/alarm")
+    public ResponseEntity<Object> PutAlarm(@RequestParam int alarm_no) {
+        log.info("alarm Status 변경, 호출한 alarmVo : {}", alarm_no);
+        service.statusUpdateAlarm(alarm_no);
+        return ResponseEntity.ok("변경완료");
+    }
+    @Operation(summary = "alarm 삭제", description = "alarm 목록의 x 클릭시")
+    @Parameter(name = "alarm_no", description = "유저 개인을 식별하기위한 유저번호")
+    @PutMapping(value = "/alarm-delete")
+    public ResponseEntity<Object> DeleteAlarm(@RequestParam int alarm_no) {
+        log.info("alarm Status 변경, 호출한 alarmVo : {}", alarm_no);
+        service.deleteAlarm(alarm_no);
+        return ResponseEntity.ok("삭제완료");
     }
 }
